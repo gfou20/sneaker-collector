acdec58d998144a7bf380b4663896566a5a75594
GFou <gfouchard20@outlook.com>
2022-11-07 18:15:34 -0500

    create sneaker index.html template

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('sneakers/', views.sneakers_index, name='sneakers_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Sneaker:  
+  def __init__(self, name, brand, description, color):
+    self.name = name
+    self.brand = brand
+    self.description = description
+    self.color = color
+
+sneakers = [
+  Sneaker('Taxi', 'Jordan', 'Jordan 1 silhoutte', 'Yellow'),
+  Sneaker('Pollen', 'Jordan', 'Jordan 1 silhoutte', 'Dark_Yellow'),
+  Sneaker('Tokyo', 'Jordan', 'Jordan 1 silhoutte', 'Silver'),
+  Sneaker('Off_White', 'Jordan', 'Jordan 1 silhoutte', 'Red, Black, White')
+]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Sneakers</h1>')
 
 def about(request):
   return render(request, 'about.html')  
+
+def sneakers_index(request):
+  return render(request, 'sneakers/index.html', {'sneakers': sneakers})
