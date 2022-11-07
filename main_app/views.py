9f0623733b98f2e0d1131d8022dd262ef48729c0
GFou <gfouchard20@outlook.com>
2022-11-07 18:53:30 -0500

    implement admin accessibility and remove fake sneaker data/model

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Sneaker:  
-  def __init__(self, name, brand, description, color):
-    self.name = name
-    self.brand = brand
-    self.description = description
-    self.color = color
-
-sneakers = [
-  Sneaker('Taxi', 'Jordan', 'Jordan 1 silhoutte', 'Yellow'),
-  Sneaker('Pollen', 'Jordan', 'Jordan 1 silhoutte', 'Dark_Yellow'),
-  Sneaker('Tokyo', 'Jordan', 'Jordan 1 silhoutte', 'Silver'),
-  Sneaker('Off_White', 'Jordan', 'Jordan 1 silhoutte', 'Red, Black, White')
-]
+from .models import Sneaker
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Sneakers</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')  
 
 def sneakers_index(request):
+  sneakers = Sneaker.objects.all()
   return render(request, 'sneakers/index.html', {'sneakers': sneakers})
