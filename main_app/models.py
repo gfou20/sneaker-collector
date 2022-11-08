9aeae90d4ed10d4d98b27fd2df06394f6de35389
GFou <gfouchard20@outlook.com>
2022-11-08 16:16:10 -0500

    create Release model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+DROP_TYPE = (
+  ('GR', 'General Release'),
+  ('LR', 'Limited Release'),
+  ('SD', 'Shock Drop')
+)
+
 # Create your models here.
 class Sneaker(models.Model):
   name = models.CharField(max_length=100)
@@ -14,3 +20,15 @@ class Sneaker(models.Model):
 
   def get_absolute_url(self):
     return reverse('sneakers_detail', kwargs={'sneaker_id': self.id})
+
+class Release(models.Model):
+  date = models.DateField('Release Date')
+  drop_type = models.CharField(
+    max_length=2,
+    choices=DROP_TYPE,
+    default=DROP_TYPE[0][0]
+  )  
+  sneaker = models.ForeignKey(Sneaker, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_drop_type_display()} on {self.date}"
